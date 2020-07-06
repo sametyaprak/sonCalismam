cf397c39527e087f972360b5e3954e625fa4b97a
sametyaprak <sametyaprak@gmail.com>
2020-07-06 20:36:42 +0200

    gdf

--- Dunya.java
+++ Dunya.java
@@ -5,15 +5,11 @@ public class Dunya {
 	public static void main(String[] args) {
 		
 		System.out.println("1");
-		System.out.println("master calismasi basladi");
-		System.out.println("remotta da duzenleme yapidi");
+		System.out.println("kkkkkkkk");
+		System.out.println("remotta da duzenleme yapidi");		
 		System.out.println("bunu master olarak kabul edin");
 		System.out.println("xxxxxx");
 		System.out.println("yyyyyy");
-		System.out.println("2");
-		System.out.println("ikinci gun calismalari");
-		System.out.println("remotta da duzenleme yapidi");
-		System.out.println("xxxxxxx");
-		System.out.println("yyyyyy");
+		
 	}
 }
